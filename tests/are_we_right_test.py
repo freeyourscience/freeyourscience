04336259135f48bdf0b0945f26340fb7cc585a87
Lukas Grossberger <code@grossberger.xyz>
2020-12-02 18:48:31 +0100

    add oa_pathway implementation against sherpa api without caching

--- tests/are_we_right_test.py
+++ tests/are_we_right_test.py
@@ -1,7 +1,10 @@
 import os
 import json
 
-from wbf.are_we_right import calculate_metrics
+import pytest
+from requests import Response
+
+from wbf.are_we_right import calculate_metrics, oa_pathway
 
 
 ASSETS_PATH = os.path.join(os.path.dirname(__file__), "assets")
@@ -17,3 +20,81 @@ def test_calculate_metrics():
     assert n_pathway_nocost == 1
     assert n_pathway_other == 1
     assert n_unknown == 1
+
+
+@pytest.mark.parametrize(
+    "issn,pathway",
+    [("1179-3163", "other"), ("2050-084X", "nocost"), ("DOESNT-EXIST", "not-found")],
+)
+def test_oa_pathway_successes(issn, pathway, monkeypatch):
+    with open(os.path.join(ASSETS_PATH, "publishers.json"), "r") as fh:
+        publishers = json.load(fh)["items"]
+
+    def mock_get_publisher(url):
+        publisher_issn = url.split('"')[-2]
+        selected_publishers = [p for p in publishers if publisher_issn in json.dumps(p)]
+        response = Response()
+        response.status_code = 200
+        response._content = json.dumps({"items": selected_publishers}).encode("utf-8")
+        return response
+
+    monkeypatch.setattr("wbf.are_we_right.requests.get", mock_get_publisher)
+
+    updated_paper = oa_pathway(
+        paper={
+            "doi": "10.1011/111111",
+            "issn": issn,
+            "unpaywall_status": "not-oa",
+        },
+        api_key="DUMMY-KEY",
+    )
+    assert updated_paper["pathway"] == pathway
+
+
+def test_oa_pathway_request_error(monkeypatch):
+    def mock_get_publisher(url):
+        response = Response()
+        response.status_code = 404
+        return response
+
+    monkeypatch.setattr("wbf.are_we_right.requests.get", mock_get_publisher)
+
+    updated_paper = oa_pathway(
+        paper={
+            "doi": "10.1011/111111",
+            "issn": "1234-1234",
+            "unpaywall_status": "not-oa",
+        },
+        api_key="DUMMY-KEY",
+    )
+    assert updated_paper["pathway"] == "not-found"
+
+
+def test_oa_pathway_irrelevant_oa_status():
+    paper = {
+        "doi": "10.1011/111111",
+        "issn": "1234-1234",
+    }
+
+    paper["unpaywall_status"] = "oa"
+    updated_paper = oa_pathway(paper=paper, api_key="DUMMY-KEY")
+    assert updated_paper["pathway"] == "already-oa"
+
+    paper["unpaywall_status"] = "not-found"
+    updated_paper = oa_pathway(paper=paper, api_key="DUMMY-KEY")
+    assert updated_paper["pathway"] == "not-attempted"
+
+
+def test_oa_pathway_with_no_api_key():
+    api_key = os.environ.pop("SHERPA_API_KEY")
+
+    with pytest.raises(RuntimeError):
+        oa_pathway(
+            paper={
+                "doi": "10.1011/111111",
+                "issn": "1234-1234",
+                "unpaywall_status": "not-oa",
+            }
+        )
+
+    api_key = os.environ["SHERPA_API_KEY"] = api_key
--- wbf/are_we_right.py
+++ wbf/are_we_right.py
@@ -1,3 +1,9 @@
+import os
+from typing import Optional
+
+import requests
+
+
 def unpaywall_status(paper: dict) -> dict:
     if paper["doi"] == "10.1011/111111":
         paper["unpaywall_status"] = "oa"
@@ -9,21 +15,66 @@ def unpaywall_status(paper: dict) -> dict:
     return paper
 
 
-def oa_pathway(paper: dict) -> dict:
+def oa_pathway(paper: dict, api_key: Optional[str] = None) -> dict:
+    """Enrich a given paper with information about the available open access pathway
+    collected from the Sherpa API (v2.sherpa.ac.uk), which is added as a "pathway" key
+    to the given dictionary that can contain any of the following values:
+      * already-oa (the paper is already available as open access)
+      * not-attempted (there is no information about the open access availability)
+      * nocost (the publisher policy allows for re-publishing without additional cost)
+      * other (the publisher policy doesn't allow for free re-publishing)
+      * not-found (no information about the publisher policy could be retreived)
+
+    TODO: Cache publisher policies.
+
+    Raises
+    ------
+    RuntimeError
+        In case no Sherpa API key is passed to the function as an argument and none is
+        found in the ``SHERPA_API_KEY`` environment variable.
+        To obtain an API key, register at https://v2.sherpa.ac.uk/cgi/register
+    """
+    api_key = os.getenv("SHERPA_API_KEY", api_key)
+    if api_key is None:
+        raise RuntimeError(
+            "No Sherpa API key available in the 'SHERPA_API_KEY' environment variable."
+        )
+
     if paper["unpaywall_status"] == "oa":
         paper["pathway"] = "already-oa"
         return paper
+
     if paper["unpaywall_status"] == "not-found":
         paper["pathway"] = "not-attempted"
         return paper
 
-    if paper["issn"] == "1234-1234":
-        paper["pathway"] = "nocost"
-    elif paper["issn"] == "1234-5678":
-        paper["pathway"] = "other"
-    else:
+    response = requests.get(
+        "https://v2.sherpa.ac.uk/cgi/retrieve?"
+        + f"item-type=publication&api-key={api_key}&format=Json&"
+        + f'filter=[["issn","equals","{paper["issn"]}"]]'
+    )
+    if not response.ok:
         paper["pathway"] = "not-found"
+        return paper
 
+    publications = response.json()
+    if not publications or not publications["items"]:
+        paper["pathway"] = "not-found"
+        return paper
+
+    # TODO: How to handle multiple publishers found for ISSN?
+
+    oa_policies_no_cost = [
+        policy
+        for policy in publications["items"][0]["publisher_policy"]
+        if policy["open_access_prohibited"] == "no"
+        and any([perm["additional_oa_fee"] == "no" for perm in policy["permitted_oa"]])
+    ]
+    if not oa_policies_no_cost:
+        paper["pathway"] = "other"
+        return paper
+
+    paper["pathway"] = "nocost"
     return paper
 
 
@@ -50,7 +101,7 @@ if __name__ == "__main__":
     input_of_papers = [
         {"doi": "10.1011/111111", "issn": "1234-1234"},
         {"doi": "10.1011/222222", "issn": "1234-1234"},
-        {"doi": "10.1011/222222", "issn": "1234-5678"},
+        {"doi": "10.1011/222222", "issn": "2050-084X"},
         {"doi": "10.1011/444444", "issn": "1234-1234"},
     ]
 
