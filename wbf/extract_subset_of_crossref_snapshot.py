bf51d3c34589051aecb5b392ba19fd9bd29f059c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-02 20:16:36 +0000

    turn loader into generator, use to extract 10k records from first 100k

--- wbf/extract_subset_of_crossref_snapshot.py
+++ wbf/extract_subset_of_crossref_snapshot.py
@@ -3,10 +3,28 @@ import tarfile
 
 SNAPSHOT_PATH = "/mnt/data/wbf/crossref-metadata-2020-10.json.tar.gz"
 
-with tarfile.open(SNAPSHOT_PATH, "r:gz") as tar:
-    for i, file in enumerate(tar):
-        if file.name.endswith(".json"):
-            data = json.loads(tar.extractfile(file).read())
-            extract = [(i['DOI'], i.get('ISSN', 'not-available')[0]) for i in data['items']]
-            print(extract)
-            break
+
+def doi_issn_loader(json_tar_gz_path):
+    with tarfile.open(json_tar_gz_path, "r:gz") as tar:
+        for file in tar:
+            if file.name.endswith(".json"):
+                data = json.loads(tar.extractfile(file).read())
+                for record in data["items"]:
+                    yield (
+                        {
+                            "doi": record["DOI"],
+                            "issn": record.get("ISSN", "not-available")[0],
+                        }
+                    )
+
+
+if __name__ == "__main__":
+    subset = []
+    doi_issn = doi_issn_loader(SNAPSHOT_PATH)
+    for i in range(1000000):
+        if i % 100 == 0:
+            subset.append(next(doi_issn))
+        else:
+            next(doi_issn)
+    with open("tests/assets/crossref_subset.json", "w") as fh:
+        json.dump(subset, fh)
