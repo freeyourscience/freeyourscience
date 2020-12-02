58395fab021fa6924a944efa73f103b912c6e771
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-02 10:28:38 +0000

    filter papers into non_oa

--- are_we_right.py
+++ are_we_right.py
@@ -1,10 +1,21 @@
 input_of_papers = [
-  {'doi':'10.1011/111111' , 'issn':'1234-1234'}
+  {'doi':'10.1011/111111' , 'issn':'1234-1234'},
+  {'doi':'10.1011/222222' , 'issn':'1234-1234'}
 ]
+
+def is_oa(paper: dict):
+  if paper['doi'] == '10.1011/111111':
+    return True
+  else:
+    return False
 
 n_pubs = len(input_of_papers)
 
-n_oa, n_pathway_nocost, n_pathway_other, n_unknown = 400, 500, 50, 50
+non_oa_papers = list(filter(is_oa, input_of_papers))
+
+n_oa = n_pubs - len(non_oa_papers)
+
+n_pathway_nocost, n_pathway_other, n_unknown = 500, 50, 50
 
 print(f"looked at {n_pubs} publications")
 print(f"{n_oa} are already OA")
