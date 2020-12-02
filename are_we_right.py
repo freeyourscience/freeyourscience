f07ddee58cf231b63ccbd02c55a89d50706ba2e3
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-02 10:18:43 +0000

    complete list of possible outputs

--- are_we_right.py
+++ are_we_right.py
@@ -1,5 +1,7 @@
-n_pubs, n_oa, n_pathway = 1000, 400, 600
+n_pubs, n_oa, n_pathway_nocost, n_pathway_other, n_unknown = 1000, 400, 500, 50, 50
 
 print(f"looked at {n_pubs} publications")
 print(f"{n_oa} are already OA")
-print(f"{n_pathway} could be OA at no cost")
+print(f"{n_pathway_nocost} could be OA at no cost")
+print(f"{n_pathway_other} could only be OA at cost")
+print(f"{n_unknown} could not be determined")
