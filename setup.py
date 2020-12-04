aa59d6b610f91dcc2ff240d27d5ab6d096e43585
Lukas Grossberger <code@grossberger.xyz>
2020-12-04 13:10:35 +0100

    add schemas for papers

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     python_requires=">=3.7, <4",
-    install_requires=["requests"],
+    install_requires=["requests", "pydantic"],
     extras_require={
         "dev": ["black", "pytest", "pytest-cov"],
     },
