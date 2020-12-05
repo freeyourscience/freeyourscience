664881cc8561ab6a3008c5d8a018980f924c48d3
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-05 22:16:55 +0000

    fix obsolete doctstring/funcname of extract_fields

--- wbf/load_from_snapshot.py
+++ wbf/load_from_snapshot.py
@@ -5,8 +5,7 @@ import tarfile
 UNPAYWALL_SNAPSHOT_PATH = "/mnt/data/wbf/unpaywall.jsonl.gz"
 
 
-def extract_doi_issn(json_loader):
-    """Yields only the doi and issn of a json record"""
+def extract_fields(json_loader):
     for record in json_loader:
         yield (
             {
@@ -25,7 +24,7 @@ def load_unpaywall_snapshot(jsonl_gzip_path):
 
 
 if __name__ == "__main__":
-    doi_issn = extract_doi_issn(load_unpaywall_snapshot(UNPAYWALL_SNAPSHOT_PATH))
+    doi_issn = extract_fields(load_unpaywall_snapshot(UNPAYWALL_SNAPSHOT_PATH))
     with open("tests/assets/unpaywall_subset.jsonl", "w") as fh:
         for i, record in enumerate(doi_issn):
             if i == 1000:
