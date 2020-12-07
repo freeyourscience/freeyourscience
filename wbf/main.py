31ff7e1d126637f620cb241d0c38c03592a260b9
Lukas Grossberger <code@grossberger.xyz>
2020-12-07 16:08:41 +0100

    add prelim favicon as example for static files

--- wbf/main.py
+++ wbf/main.py
@@ -1,11 +1,18 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 from wbf.api import api_router
 
-app = FastAPI()
 
+STATIC_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "templates")
+
+app = FastAPI()
 app.include_router(api_router)
+app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
