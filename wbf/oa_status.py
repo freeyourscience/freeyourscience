0145f2ca9e1a402989896a8a2d2f9805d3d948de
Lukas Grossberger <code@grossberger.xyz>
2020-12-09 10:02:04 +0100

    add s2 api request to validate unpaywall not oa or not found status

--- wbf/oa_status.py
+++ wbf/oa_status.py
@@ -1,5 +1,6 @@
-from wbf.schemas import Paper, PaperWithOAStatus
+from wbf.schemas import Paper, PaperWithOAStatus, OAStatus
 from wbf.unpaywall import get_oa_status_and_issn as unpaywall_status_api
+from wbf.semantic_scholar import get_paper as s2_get_paper
 
 
 def oa_status(paper: Paper) -> PaperWithOAStatus:
@@ -7,5 +8,10 @@ def oa_status(paper: Paper) -> PaperWithOAStatus:
     copy collected from the an unpaywall data dump or the unpaywall API.
     """
     oa_status, _ = unpaywall_status_api(paper.doi)
+
+    if oa_status is not OAStatus.oa:
+        s2_paper = s2_get_paper(paper.doi)
+        if s2_paper is not None and s2_paper.is_open_access:
+            oa_status = OAStatus.oa
 
     return PaperWithOAStatus(oa_status=oa_status, **paper.dict())
