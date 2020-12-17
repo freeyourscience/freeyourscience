ea62d873ec1e2a4d78fd4f9b21402aaa4df00af9
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-17 21:32:50 +0000

    extract requirements into own files too allow for optimized docker build

--- setup.py
+++ setup.py
@@ -1,23 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+with open("requirements_dev.txt") as f:
+    extras_require = {"dev": f.read().splitlines()}
+
 setup(
     name="fyscience",
     version="0.0.1",
     packages=find_packages(),
     python_requires=">=3.7, <4",
-    install_requires=[
-        "requests",
-        "pydantic[dotenv]",
-        "fastapi",
-        "uvicorn",
-        "gunicorn",
-        "jinja2",
-        "aiofiles",
-        "uvloop",
-        "httptools",
-        "loguru",
-    ],
-    extras_require={
-        "dev": ["black", "pytest", "pytest-cov", "pytest-mock"],
-    },
+    install_requires=install_requires,
+    extras_require=extras_require,
 )
