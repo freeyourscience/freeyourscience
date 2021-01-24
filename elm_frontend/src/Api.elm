230fca9ff61f6a43781a69c258874ae2c576985b
Lukas Grossberger <code@grossberger.xyz>
2021-01-24 14:19:02 +0100

    fix policy details notes field to public notes in decoder #20

--- elm_frontend/src/Api.elm
+++ elm_frontend/src/Api.elm
@@ -60,7 +60,7 @@ policyDetailsDecoder =
         |> required "urls" (D.nullable (D.list namedUrlDecoder))
         |> required "permitted_oa" (D.nullable (D.list permittedOADecoder))
         |> required "uri" D.string
-        |> optional "notes" (D.nullable D.string) Nothing
+        |> optional "public_notes" (D.nullable D.string) Nothing
 
 
 paperDecoder : Decoder BackendPaper
