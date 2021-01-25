76e9c7a16f649e8c7e881292e5c475bdcb72c0e5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-25 22:40:14 +0000

    extract renderPaperMetaData

--- elm_frontend/src/GeneralTypes.elm
+++ elm_frontend/src/GeneralTypes.elm
@@ -1,7 +1,7 @@
-module GeneralTypes exposing (DOI, NamedUrl, PaperMetadata, renderUrl)
+module GeneralTypes exposing (DOI, NamedUrl, PaperMetadata, renderPaperMetaData, renderUrl)
 
-import Html exposing (Html, a, text)
-import Html.Attributes exposing (class, href)
+import Html exposing (Html, a, div, img, text)
+import Html.Attributes exposing (alt, class, height, href, src, target, width)
 
 
 type alias DOI =
@@ -27,3 +27,32 @@ type alias NamedUrl =
 renderUrl : NamedUrl -> Html msg
 renderUrl { url, description } =
     a [ href url, class "link", class "link-secondary" ] [ text description ]
+
+
+renderPaperMetaData : PaperMetadata -> List (Html msg)
+renderPaperMetaData { title, journal, authors, year, doi } =
+    [ div [ class "fs-5 mb-1" ] [ text (Maybe.withDefault "Unknown title" title) ]
+    , div [ class "mb-1" ]
+        [ text
+            (String.concat
+                [ journal |> Maybe.withDefault "Unknown journal"
+                , ", "
+                , authors |> Maybe.withDefault "Unknown authors"
+                , " ("
+                , year |> Maybe.map String.fromInt |> Maybe.withDefault ""
+                , "), "
+                , doi
+                ]
+            )
+        , a [ href ("https://doi.org/" ++ doi), class "link-secondary", target "_blank" ]
+            [ img
+                [ src "/static/img/box-arrow-up-right.svg"
+                , alt ""
+                , width 12
+                , height 12
+                , Html.Attributes.title ("Visit article: " ++ Maybe.withDefault "" title)
+                ]
+                []
+            ]
+        ]
+    ]
